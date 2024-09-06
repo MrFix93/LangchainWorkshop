983ce5d94d825847cb7f8704f5ec99fc0346fd99
Michael Koers <michael.koers@infosupport.com>
2024-09-06 11:59:45 +0200

    Verwijder RAG

--- src/main/java/nl/infosupport/swagshop/Configuration.java
+++ src/main/java/nl/infosupport/swagshop/Configuration.java
@@ -17,8 +17,6 @@ public class Configuration {
 
     static Assistant assistant() {
 
-        var embeddingStore = loadEmbeddingStore("src/main/resources/voorwerpen");
-
         return AiServices.builder(Assistant.class)
                 // Het bouwen van de Large Language Model, in dit geval is er gekozen voor OpenAI's ChatGPT
                 .chatLanguageModel(OpenAiChatModel.builder()
@@ -30,16 +28,8 @@ public class Configuration {
                 // Een system message, hiermee geven we de chatbot wat achtergrond informatie en hoe deze moet reageren
                 .systemMessageProvider((var x) -> "Je bent de chatbot voor de Info Support swag shop, een digitale winkel! Houdt antwoorden vriendelijk maar kort")
                 .tools(new KlantTools())
-                .contentRetriever(EmbeddingStoreContentRetriever.from(embeddingStore))
                 // In-memory chat geheugen, zodat chatbot context heeft van afgelopen 10 berichten.
                 .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
                 .build();
-    }
-
-    private static InMemoryEmbeddingStore<TextSegment> loadEmbeddingStore(String path) {
-        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-        List<Document> documents = FileSystemDocumentLoader.loadDocuments(path);
-        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
-        return embeddingStore;
     }
 }
