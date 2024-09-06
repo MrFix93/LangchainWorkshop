c8257ac1fe0f8f9a374db5771374b8b0b1eb803c
Michael Koers <michael.koers@infosupport.com>
2024-09-06 13:08:04 +0200

    Main leegmaken voor opdrachten

--- src/main/java/nl/infosupport/swagshop/KlantTools.java
+++ src/main/java/nl/infosupport/swagshop/KlantTools.java
@@ -10,49 +10,5 @@ import nl.infosupport.swagshop.model.Product;
 class KlantTools {
 
     private static final Database database = Database.init();
-    private static final double TAX = 1.21d;
 
-    @Tool("Creates and returns a new customer")
-    Customer createCustomer(@P("Customer name") String name) {
-        return database.createNewCustomer(name);
-    }
-
-    @Tool("Searches and returns a customer")
-    Customer getCustomerByName(@P("Customer name") String name) {
-        return database.getCustomerByName(name);
-    }
-
-    @Tool("Searches for product by name")
-    Product getProductByName(@P("Product name") String name) {
-        return database.getProductByName(name);
-    }
-
-    @Tool("Create a new order for Customer")
-    int createOrder(@P("Customer with ID and Name") Customer customer,
-                    @P("Requested product") Product product,
-                    int quantity) {
-        return database.createNewOrder(customer, new OrderLine(quantity, product));
-    }
-
-    @Tool("Add a new orderline to an existing order, returns boolean indicating success or failure")
-    boolean updateOrder(@P("The existing order ID") int orderId,
-                        @P("Requested product") Product product,
-                        @P("The amount") int quantity) {
-        return database.updateOrder(orderId, new OrderLine(quantity, product));
-    }
-
-    @Tool("Get details of an order")
-    Order getOrderById(@P("Order ID") int id) {
-        return database.getOrderById(id);
-    }
-
-    @Tool("Calculates totale price of order including tax")
-    double getTotalOrderPrice(@P("Order ID") int id) {
-        return database.getOrderById(id)
-                .orderLines()
-                .stream()
-                .mapToDouble(o -> o.product().price() * o.quantity())
-                .map(p -> p * TAX)
-                .sum();
-    }
 }
--- src/main/java/nl/infosupport/swagshop/Main.java
+++ src/main/java/nl/infosupport/swagshop/Main.java
@@ -1,24 +1,8 @@
 package nl.infosupport.swagshop;
 
-import java.util.Scanner;
-
 
 public class Main {
     public static void main(String[] args) {
-        Assistant assistant = Configuration.assistant();
 
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Dit is het begin van jouw gesprek met de Info Support swag shop chatbot!");
-        System.out.print("[input]: ");
-
-        while (scanner.hasNext()) {
-            String in = scanner.nextLine();
-
-            String answer = assistant.chat(in);
-            System.out.println("[output]: " + answer);
-            System.out.println();
-            System.out.print("[input]: ");
-        }
     }
 }
