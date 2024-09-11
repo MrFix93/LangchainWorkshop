d57f0d0a02ee9b1f5c681d47c0aafcd625a1e21a
Michael <michaelkoers95@gmail.com>
2024-09-11 19:09:41 +0200

    Java dev container toegevoegd, lose langchain imports verwijderd

--- src/main/java/nl/infosupport/swagshop/KlantTools.java
+++ src/main/java/nl/infosupport/swagshop/KlantTools.java
@@ -1,12 +1,5 @@
 package nl.infosupport.swagshop;
 
-import dev.langchain4j.agent.tool.P;
-import dev.langchain4j.agent.tool.Tool;
-import nl.infosupport.swagshop.model.Customer;
-import nl.infosupport.swagshop.model.Order;
-import nl.infosupport.swagshop.model.OrderLine;
-import nl.infosupport.swagshop.model.Product;
-
 class KlantTools {
 
     private static final Database database = Database.init();
